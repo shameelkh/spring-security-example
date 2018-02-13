184e1365ee6cc1373f68ddfdac01857aa6033a25
shameelkh <shameel-khan@hotmail.com>
2018-02-12 19:55:04 -0500

    Add db User entity and repository

---
+++ src/main/java/playground/db/User.java
@@ -0,0 +1,24 @@
+package playground.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+}
